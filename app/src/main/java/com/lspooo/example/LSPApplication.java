6ba6e2e0d976660cf0ca8f150bf15fd7a112aae6
林仕鹏的电脑\LSP <lsp929488@163.com>
2017-09-21 16:02:54 +0800

    配置更新

---
+++ app/src/main/java/com/lspooo/example/LSPApplication.java
@@ -0,0 +1,15 @@
+package com.lspooo.example;
+
+import android.app.Application;
+
+/**
+ * Created by LSP on 2017/9/21.
+ */
+
+public class LSPApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/lspooo/example/ui/LauncherUI.java
@@ -0,0 +1,19 @@
+package com.lspooo.example.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.lspooo.example.R;
+
+/**
+ * Created by LSP on 2017/9/21.
+ */
+
+public class LauncherUI extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
